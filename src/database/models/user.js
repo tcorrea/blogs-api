6441d9347211fd44ba64cf581cc45fcbab814f38
Thiago Corrêa <thiagocorreacontato@gmail.com>
2022-07-13 17:54:17 -0300

    feat(user-model): requisito 02 - user model criado o user model

---
+++ src/database/models/user.js
@@ -0,0 +1,27 @@
+const sequelize = require("sequelize");
+
+const createUser = (sequelize, DataTypes) => {
+
+  const MODEL_NAME = 'User';
+
+  const attributes = {
+    id: DataTypes.INTEGER,
+    email: DataTypes.STRING,
+    password: DataTypes.STRING,
+    displayName: DataTypes.STRING,
+    email: DataTypes.STRING,
+    image: DataTypes.STRING,
+  };
+
+  const options = { tableName: 'Users' };
+
+  const User = sequelize.define(MODEL_NAME, attributes, options);
+
+  // User.associate = (db) => {
+  //   User.hasMany(db.Pet, { as: 'pets', foreignKey: 'userId' });
+  // }
+
+  return User;
+};
+
+module.exports = createUser;
